cb58027e20bbedadd104ef74f742088bed4745b2
TofikZeMoSo <tofik.maniyar@zemosolabs.com>
2022-03-17 09:22:37 +0530

    Assignment10

---
+++ Assignment10/src/Node.java
@@ -0,0 +1,9 @@
+public class Node <T>{
+
+        T num;
+        Node <T> next;
+        Node(T num) {
+            this.num=num;
+        }
+    }
+
---
+++ Assignment10/src/SList.java
@@ -0,0 +1,10 @@
+public class SList <T> {
+    Node <T> head;
+
+    SList() {
+        this.head = null;
+    }
+    public SListIterator iterator() {
+        return new SListIterator(head);
+    }
+}
